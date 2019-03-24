cd51da9ddda3a7d72277cd49d250839a44f8292e
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-24 15:54:16 -0500

    using select to project data lesson complete.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -22,7 +22,18 @@ namespace Cars
                 from car in cars
                 where car.Manufacturer == "BMW" && car.Year == 2016
                 orderby car.Combined descending, car.Name ascending
-                select car;
+                // creating an anonymous object
+                select new
+                {
+                    car.Manufacturer,
+                    car.Name,
+                    car.Combined
+                };
+
+            //could also use All() or Contains() extension methods to produce the same result
+            var result = cars.Any(c => c.Manufacturer == "Ford");
+
+            Console.WriteLine(result);
 
             //extension method syntax with lambda expressions
             //var query2 =
@@ -32,19 +43,54 @@ namespace Cars
 
             foreach (var car in query.Take(10))
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{car.Manufacturer} : {car.Name} : {car.Combined}");
             }
             Console.ReadLine();
         }
 
         private static List<Car> ProcessFile(string path)
         {
+            //using query syntax
+            //var query =
+            //    from line in File.ReadAllLines(path).Skip(1)
+            //    where line.Length > 1
+            //    select Car.ParseFromCsv(line);
+
+            //return query.ToList();
+
+            //using extension method syntax
             var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(l => l.Length > 1)
+                .ToCar();
 
             return query.ToList();
+
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+
+                };
+            }
         }
     }
 }
