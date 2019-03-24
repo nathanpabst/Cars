ad5b2746b3b87c758e5ca9c3aaec72a226973cc0
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-24 15:15:14 -0500

    secondary sort by car name in place and working as expected.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -12,21 +12,25 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel2.csv");
-            foreach (var car in cars)
+
+            var query = cars.OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name);
+
+            foreach (var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name} : {car.Combined}");
             }
             Console.ReadLine();
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            return
-            File.ReadAllLines(path)
-                .Skip(1)
-                .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv)
-                .ToList();
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            return query.ToList();
         }
     }
 }
