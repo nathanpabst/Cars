a18137536bbcd656b1d6d66dc984d81f434b5a57
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-26 19:55:02 -0500

    joined the csv files using linq.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -18,39 +18,24 @@ namespace Cars
             //var query = cars.OrderByDescending(c => c.Combined)
             //                .ThenBy(c => c.Name);
 
-            //using query syntax
+            //using query syntax && joining the csv files via linq
             var query =
                 from car in cars
-                where car.Manufacturer == "BMW" && car.Year == 2016
+                join manufacturer in manufacturers 
+                    on car.Manufacturer equals manufacturer.Name
                 orderby car.Combined descending, car.Name ascending
                 // creating an anonymous object
                 select new
                 {
-                    car.Manufacturer,
+                    manufacturer.Headquarters,
                     car.Name,
                     car.Combined
                 };
 
-            //using select many
-            var result = cars.SelectMany(c => c.Name)
-                             .OrderBy(c => c);
-
-            foreach (var character in result)
-            {               
-                Console.WriteLine(character);
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Headquarters} : {car.Name} : {car.Combined}");
             }
-
-
-            //extension method syntax with lambda expressions
-            //var query2 =
-            //    cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-            //        .OrderByDescending(c => c.Combined)
-            //        .ThenBy(c => c.Name);
-
-            //foreach (var car in query.Take(10))
-            //{
-            //    Console.WriteLine($"{car.Manufacturer} : {car.Name} : {car.Combined}");
-            //}
             Console.ReadLine();
         }
 
