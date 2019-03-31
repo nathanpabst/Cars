1f64c6383f371fbc57f9931ae109b2663ddc35dd
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-31 18:34:16 -0500

    using groupJoin to include headquarters info.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -15,21 +15,19 @@ namespace Cars
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query =
-                from car in cars
-                group car by car.Manufacturer.ToUpper() into manufacturer
-                orderby manufacturer.Key
-                select manufacturer;
+                from manufacturer in manufacturers
+                join car in cars on manufacturer.Name equals car.Manufacturer
+                into carGroup
+                select new
+                {
+                    Manufacturer = manufacturer,
+                    Cars = carGroup
+                };
 
-            //using extension method
-            var query2 =
-                cars.GroupBy(c => c.Manufacturer.ToUpper())
-                .OrderBy(g => g.Key);
-
-
-            foreach (var group in query2)
+            foreach (var group in query)
             {
-                Console.WriteLine(group.Key);
-                foreach (var car in group.OrderByDescending(c => c.Combined).Take(2))
+                Console.WriteLine($"{group.Manufacturer.Name} : {group.Manufacturer.Headquarters}");
+                foreach (var car in group.Cars.OrderByDescending(c => c.Combined).Take(2))
                 {
                     Console.WriteLine($"\t{car.Name} : {car.Combined}");
                 }
