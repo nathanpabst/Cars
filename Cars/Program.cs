edf4897c8723625f6332dba1470e55f91cfd640b
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-31 18:40:49 -0500

    same query using the extension method syntax

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -18,13 +18,24 @@ namespace Cars
                 from manufacturer in manufacturers
                 join car in cars on manufacturer.Name equals car.Manufacturer
                 into carGroup
+                orderby manufacturer.Name
                 select new
                 {
                     Manufacturer = manufacturer,
                     Cars = carGroup
                 };
 
-            foreach (var group in query)
+            //using extension method syntax
+            var query2 =
+                manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) =>
+                    new
+                    {
+                        Manufacturer = m,
+                        Cars = g
+                    })
+                .OrderBy(m => m.Manufacturer.Name);
+
+            foreach (var group in query2)
             {
                 Console.WriteLine($"{group.Manufacturer.Name} : {group.Manufacturer.Headquarters}");
                 foreach (var car in group.Cars.OrderByDescending(c => c.Combined).Take(2))
